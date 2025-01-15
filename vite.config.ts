f355681d610f1710516bb568dd668678efdac850
Parambrata Ghosh <gpampa138@gmail.com>
2025-01-15 20:43:45 +0530

    initialize project with Vite, React, and TypeScript setup

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import fs from 'fs'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  
+  server: {
+    host: '192.168.0.193',
+    // host: 'localhost'
+    // host: '127.0.0.1',
+    https: {
+      cert: fs.readFileSync('localhost.pem'),
+      key: fs.readFileSync('localhost-key.pem')
+    }
+  }
+})
