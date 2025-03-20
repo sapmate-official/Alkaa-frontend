b5e0444d4c0b32207564537a4faee268ef210b9d
Parambrata Ghosh <gpampa138@gmail.com>
2025-03-20 12:41:13 +0530

    feat: add AlertDialog component using Radix UI for improved user interaction

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -259,26 +259,28 @@ export const Logo = () => {
   return (
     <Link
       to="/"
-      className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
+      className="flex items-center justify-start py-2 relative z-20"
     >
-      <div className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" />
-      <motion.span
+      <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        className="font-medium text-black dark:text-white whitespace-pre"
+        className="h-12 w-32"
       >
-        Alkaa
-      </motion.span>
+        <img src="/logo.svg" alt="Alkaa Logo" className="h-full w-full object-contain" />
+      </motion.div>
     </Link>
   );
 };
+
 export const LogoIcon = () => {
   return (
     <Link
-      to="#"
-      className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
+      to="/p/"
+      className="flex items-center justify-center py-2 relative z-20"
     >
-      <div className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" />
+      <div className="h-8 w-8">
+        <img src="/logo_icon.svg" alt="Alkaa Logo" className="h-full w-full object-contain" />
+      </div>
     </Link>
   );
 };
