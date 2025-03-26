25f3f3bf5859d40093747fc19684ed2ce96c7df1
Parambrata Ghosh <gpampa138@gmail.com>
2025-03-26 19:51:08 +0530

    feat: update login form with logo and new background styling; add login cover image

--- src/page/public/auth/SignIn.tsx
+++ src/page/public/auth/SignIn.tsx
@@ -16,8 +16,9 @@ export default function SignIn() {
     return <Loader/>
   }
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center bg-gray-100 p-6 md:p-10">
+    <div className="flex min-h-svh flex-col items-center justify-center bg-background p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
+        
         <LoginForm />
       </div>
     </div>
