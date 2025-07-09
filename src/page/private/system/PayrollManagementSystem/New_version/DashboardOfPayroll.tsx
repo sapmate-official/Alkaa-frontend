a457819458c87bd6c5cabab11a35f191d749de96
param2610-cloud <gpampa138@gmail.com>
2025-07-09 20:15:05 +0530

    [STABLE RELEASE] refactor: Remove unused import of useNavigate from DashboardOfPayroll component

--- src/page/private/system/PayrollManagementSystem/New_version/DashboardOfPayroll.tsx
+++ src/page/private/system/PayrollManagementSystem/New_version/DashboardOfPayroll.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react'
 import { useAuth } from '@/services/AuthContext'
-import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import { APIV3Dictionary } from '@/api/v3/Api3Dicts'
 import { useAtom } from 'jotai'
